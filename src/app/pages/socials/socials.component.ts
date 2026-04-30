cd47da420bf0bf3256c26d282de2120675c794d3
vans00-prog <ivanvodnizkiy@gmail.com>
2026-04-30 23:54:06 +0300

    refactor: update branding from Horeca to Golden Ring across the application

--- src/app/pages/socials/socials.component.ts
+++ src/app/pages/socials/socials.component.ts
@@ -1,10 +1,39 @@
 import { NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { TranslatePipe, TranslateService } from '@wawjs/ngx-translate';
+import { LanguageService } from '../../feature/language/language.service';
+
+const SOCIALS_SEO_TITLE_KEY = 'SEO Socials Title';
+const SOCIALS_SEO_DESCRIPTION_KEY = 'SEO Socials Description';
 
 @Component({
-	imports: [NgOptimizedImage],
+	imports: [NgOptimizedImage, TranslatePipe],
 	templateUrl: './socials.component.html',
 	styleUrl: './socials.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SocialsComponent {}
+export class SocialsComponent {
+	private readonly _title = inject(Title);
+	private readonly _meta = inject(Meta);
+	private readonly _translateService = inject(TranslateService);
+	private readonly _languageService = inject(LanguageService);
+
+	constructor() {
+		effect(() => {
+			this._languageService.language();
+
+			const title = this._translateService.translate(SOCIALS_SEO_TITLE_KEY)();
+			const description = this._translateService.translate(SOCIALS_SEO_DESCRIPTION_KEY)();
+
+			this._title.setTitle(title);
+			this._meta.updateTag({ name: 'description', content: description });
+			this._meta.updateTag({ property: 'og:title', content: title });
+			this._meta.updateTag({ property: 'og:description', content: description });
+			this._meta.updateTag({ name: 'twitter:title', content: title });
+			this._meta.updateTag({ name: 'twitter:description', content: description });
+			this._meta.updateTag({ itemprop: 'name', content: title });
+			this._meta.updateTag({ itemprop: 'description', content: description });
+		});
+	}
+}
